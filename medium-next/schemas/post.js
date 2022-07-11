d18d286c3f59d80254c713b66736f5886d63ae0e
Bishesh <bishesh.sunam@gmail.com>
2022-07-11 20:53:39 +1200

    configured sanity & SSR

--- medium-next/schemas/post.js
+++ medium-next/schemas/post.js
@@ -6,6 +6,12 @@ export default {
     {
       name: 'title',
       title: 'Title',
+      description: "keep it short",
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
       type: 'string',
     },
     {
